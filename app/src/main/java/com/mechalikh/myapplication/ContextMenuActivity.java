8aefd2743f78813c28aea800b5a9b23b2e56708d
Charafeddine <meccharef@gmail.com>
2023-04-24 10:06:50 +0100

    Initial commit

---
+++ app/src/main/java/com/mechalikh/myapplication/ContextMenuActivity.java
@@ -0,0 +1,59 @@
+package com.mechalikh.myapplication;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ContextMenuActivity extends AppCompatActivity {
+        private RecyclerView mRecyclerView;
+        private MyAdapter mAdapter;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_context_menu);
+
+            mRecyclerView = findViewById(R.id.recycler_view);
+            mAdapter = new MyAdapter(Arrays.asList("Item 1", "Item 2", "Item 3"));
+            mRecyclerView.setAdapter(mAdapter);
+
+            mAdapter.setActionModeCallback(new ActionMode.Callback() {
+                @Override
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    getMenuInflater().inflate(R.menu.contextual_menu, menu);
+                    return true;
+                }
+
+                @Override
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                @Override
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    if (item.getItemId() == R.id.action_delete) {
+                        List<String> selectedItems = mAdapter.getSelectedItems();
+                        mAdapter.removeSelectedItems();
+                        Toast.makeText(ContextMenuActivity.this, selectedItems.size() + " items deleted", Toast.LENGTH_SHORT).show();
+                        mode.finish();
+                        return true;
+                    }
+                    return false;
+                }
+
+                @Override
+                public void onDestroyActionMode(ActionMode mode) {
+                    mAdapter.clearSelections();
+                }
+            });
+        }
+
+    }
---
+++ app/src/main/java/com/mechalikh/myapplication/MyAdapter.java
@@ -0,0 +1,132 @@
+package com.mechalikh.myapplication;
+
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private SparseBooleanArray mSelectedItems;
+    private ActionMode.Callback mActionModeCallback;
+
+    public MyAdapter(List<String> data) {
+        mData = data;
+        mSelectedItems = new SparseBooleanArray();
+    }
+
+    public void toggleSelection(int position) {
+        if (mSelectedItems.get(position, false)) {
+            mSelectedItems.delete(position);
+        } else {
+            mSelectedItems.put(position, true);
+        }
+        notifyItemChanged(position);
+    }
+
+    public void clearSelections() {
+        mSelectedItems.clear();
+        notifyDataSetChanged();
+    }
+
+    public int getSelectedItemCount() {
+        return mSelectedItems.size();
+    }
+
+    public List<Integer> getSelectedItemPositions() {
+        List<Integer> selectedItems = new ArrayList<>(mSelectedItems.size());
+        for (int i = 0; i < mSelectedItems.size(); i++) {
+            selectedItems.add(mSelectedItems.keyAt(i));
+        }
+        return selectedItems;
+    }
+
+    public List<String> getSelectedItems() {
+        List<String> selectedItems = new ArrayList<>(mSelectedItems.size());
+        for (int i = 0; i < mSelectedItems.size(); i++) {
+            selectedItems.add(mData.get(mSelectedItems.keyAt(i)));
+        }
+        return selectedItems;
+    }
+
+    public void removeSelectedItems() {
+        List<Integer> selectedItems = getSelectedItemPositions();
+        Collections.sort(selectedItems, Collections.reverseOrder());
+        for (int position : selectedItems) {
+            mData.remove(position);
+            notifyItemRemoved(position);
+        }
+        mSelectedItems.clear();
+    }
+
+    public void setActionModeCallback(ActionMode.Callback callback) {
+        mActionModeCallback = callback;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(mData.get(position));
+        holder.itemView.setActivated(mSelectedItems.get(position, false));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+        TextView textView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.text_view);
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (mSelectedItems.size() > 0) {
+                toggleSelection(position);
+            } else {
+                // Handle item click event here
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            int position = getAdapterPosition();
+            if (mSelectedItems.size() == 0) {
+                toggleSelection(position);
+                // Start the action mode
+                androidx.appcompat.view.ActionMode actionMode = ((AppCompatActivity) v.getContext()).startSupportActionMode(mActionModeCallback);
+                if (actionMode != null) {
+                    // Set the title of the action mode to the number of selected items
+                    actionMode.setTitle(String.valueOf(getSelectedItemCount()));
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
